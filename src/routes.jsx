933a7a1f8c9f5d47ee933e92ab4b9f3ce79be226
jason <jason.aghedo@gmail.com>
2023-09-16 22:33:54 +0100

    Check error

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import MovieDetails from "./pages/movie-details";
 import NotFoundPage from "./pages/NotFoundPage";
-import Home from "./pages/Home";
+import Home from "./pages/home";
 const router = createBrowserRouter([
 	{
 		path: "/",
