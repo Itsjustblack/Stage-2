1ecaf2fed963261ba1127821e0fe83b60b234cdb
jason <jason.aghedo@gmail.com>
2023-09-16 22:31:05 +0100

    Refactor

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import { QueryCache, QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
+import { QueryClient, QueryClientProvider, QueryCache } from "@tanstack/react-query";
+import { RouterProvider } from "react-router-dom";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import router from "./routes.jsx";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
-import "./index.css";
-import App from "./app";
 
 const queryClient = new QueryClient({
 	queryCache: new QueryCache({
@@ -17,7 +18,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<QueryClientProvider client={queryClient}>
 			<ToastContainer />
-			<App />
+			<RouterProvider router={router} />
 			<ReactQueryDevtools />
 		</QueryClientProvider>
 	</React.StrictMode>
